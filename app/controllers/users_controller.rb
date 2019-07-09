a3551eab7fc05a9e31153fad06af0a302bf536e2
Federica Macchi <federica.macchi3@gmail.com>
2019-07-09 10:18:23 +0100

    Started crud on user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,28 @@ class UsersController < ApplicationController
         @users = User.all
     end
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+
+
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password_digest)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :cocktails
-  resources :cocktail_ingredients
-  resources :types
-  resources :ingredients
-  resources :comments
-  resources :likes
   resources :users
+  # resources :cocktails
+  # resources :cocktail_ingredients
+  # resources :types
+  # resources :ingredients
+  # resources :comments
+  # resources :likes
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome', to: 'application#welcome'
 end
